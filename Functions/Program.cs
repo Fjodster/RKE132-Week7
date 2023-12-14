b4732a1ee3e9e4ffe44ba7a10f3a87dd24b0790c
Fjodor Pasitsnik <fjodor.pasitsnik@tktk.ee>
2023-12-14 16:27:39 -0200

    add week 7 code

---
+++ Functions/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Are you coming or leavingg & (in/out)");
+string UserChoice = Console.ReadLine();
+
+if(UserChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+
+static void PrintHello() //finction//method
+{
+Console.WriteLine("Hello, world");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, aligator");
+}
